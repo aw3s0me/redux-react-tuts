e21d8ee656c50b6678403b3779d665a0155baee6
Aleksander Korovin <akorovin00@gmail.com>
2016-06-02 17:50:44 +0200

    Basic UI for voting. (Added dumb voting pair, voting component)

---
+++ voting-client/src/index.jsx
@@ -0,0 +1,19 @@
+console.log('I am alive')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+//TODO: Hard coded. Later substitute to real pair
+const pair = ['Trainspotting', '28 Days Later']
+
+/**
+ * Voting component takes pair of entries as props
+ *
+ * Mounted and rendered in #app div in index.html
+ * @type {Object}
+ */
+ReactDOM.render(
+    <Voting pair={pair} />,
+    document.getElementById('app')
+)
